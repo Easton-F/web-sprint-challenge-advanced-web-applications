e055e8a92db94aeba5c151f6828b7720a08aebad
Easton <easton.fritsch21@gmail.com>
2023-08-29 18:47:44 -0600

    WIP

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 import PT from 'prop-types'
+import { getArticles } from '../../backend/helpers'
 
 export default function Articles(props) {
   // ✨ where are my props? Destructure them here
+  const { getArticles } = props
 
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
+    getArticles();
   })
 
   return (
